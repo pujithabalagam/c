6c872617dd06aedbee9a5421026d80887cb9dd3e
pujithabalagam <balagampujitha91@gmail.com>
2024-09-30 15:06:07 +0530

    Create Perimeter.c

---
+++ Perimeter.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+
+#include<math.h>
+
+void main()
+
+ {
+
+
+int 1,b, perimeter;
+
+printf("Enter the length and breadth ");
+
+scanf("%d%d",&1,&b);
+
+perimeter=2*(1+b);
+
+printf("the perimeter of rectangle is %d", perimeter);
+
+}
