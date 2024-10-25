15f303b5a3f1c4eaff1554b77232cb7ad1935b8e
pujithabalagam <balagampujitha91@gmail.com>
2024-10-25 12:04:36 +0530

    Create elseif.c

---
+++ elseif.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+int i;
+printf("enter i value:");
+scanf("%d",&i);
+if(i>0)
+{
+printf("i is positive");
+}
+else if(i==0)
+{
+    printf("i is equal to zero");
+}
+else
+{
+    printf("i is negative");
+}
+
+} 
